87149e5fc8cab4cdd184a2ad1484466e2e65012b
Tanmay Sinha <alokratn001@gmail.com>
2020-11-01 15:49:08 +0530

    changed styling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Add and Pass', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
@@ -89,7 +89,6 @@ function init() {
     activePlayer = 0;
     roundScore = 0;
 
-    // document.querySelector("#current-" + activePlayer).textContent = dice;
     document.querySelector(".dice").style.display = "none";
     document.getElementById("score-0").textContent = "0";
     document.getElementById("score-1").textContent = "0";
